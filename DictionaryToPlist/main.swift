643ba40e9c10475c767aace10547d8ebd9f9c408
Pedro Howat <pedro@instant.com.br>
2016-06-30 22:09:14 -0300

    Initial Commit

---
+++ DictionaryToPlist/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DictionaryToPlist
+//
+//  Created by Pedro Rodrigues on 6/30/16.
+//  Copyright © 2016 Pedro Howat. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
